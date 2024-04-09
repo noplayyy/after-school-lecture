b7b93fb7eb17fa304c2a8e286a118fef5bc54121
noplayyy <noplay1016@gmail.com>
2024-04-09 11:10:21 +0900

    add: 24.04.08 Typography > FontDescription 정의
    TextStyle 별 속성 값을 담는 struct

---
+++ 24.04.08/DesignSystem-Typography/DesignSystem-Typography/Typography/FontDescription.swift
@@ -0,0 +1,16 @@
+//
+//  FontDescription.swift
+//  DesignSystem-Typography
+//
+//  Created by Yeonju on 4/7/24.
+//
+
+import Foundation
+import UIKit
+
+struct FontDescription {
+    let font: ASFont
+    let size: CGFloat
+    let lineHeight: CGFloat
+    let style: UIFont.TextStyle
+}
