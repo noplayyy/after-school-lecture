df9c247e375c72b192706b8ada070c8355e034c4
noplayyy <noplay1016@gmail.com>
2024-04-09 11:08:08 +0900

    add: 24.04.08 Typography > Inter Font 추가

---
+++ 24.04.08/DesignSystem-Typography/DesignSystem-Typography/DesignSystem_TypographyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignSystem_TypographyApp.swift
+//  DesignSystem-Typography
+//
+//  Created by Yeonju on 4/7/24.
+//
+
+import SwiftUI
+
+@main
+struct DesignSystem_TypographyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
